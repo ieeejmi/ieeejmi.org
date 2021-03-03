7a48273eeff4dad0c0e51e8bcf9378ce8dd7e9f2
Alisamar Husain <zrthxn@gmail.com>
2021-03-04 02:01:44 +0530

    Delete deliver

--- deliver.sh
+++
@@ -1,6 +0,0 @@
-zip -r build.zip build
-
-sudo scp -i ../zxaws.pem build.zip zrthxn@zrthxn.com:/home/zrthxn/zxweb
-sudo ssh -i ../zxaws.pem zrthxn@zrthxn.com "cd zxweb; unzip build.zip"
-
-rm build.zip
